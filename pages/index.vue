a8f8831c3621bcba2571453f79ae442bb0bb8bf4
tttom3669 <tttom3669@gmail.com>
2024-11-10 22:18:14 +0800

    update: init

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+  const route = useRoute();
+</script>
+
+<template>
+  <div>
+    <h1>Page: 首頁</h1>
+    <p>目前的路徑 - route.fullPath 為 {{  route.fullPath }}</p>
+  </div>
+</template>
+
+<style scoped></style>
